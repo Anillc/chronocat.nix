71378b7f2c63db6983f343c2b3989ab6c51f9805
Anillc <i@anillc.cn>
2023-08-18 21:00:54 +0800

    migrate to flake parts

--- flake.nix
+++ flake.nix
@@ -1,8 +1,16 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in {
-    packages.x86_64-linux.default = pkgs.callPackage ./chronocat.nix {};
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
   };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.callPackage ./chronocat.nix {};
+      };
+    };
 }
